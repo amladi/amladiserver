4dc1d59cbb9ddf8f6551af829e1c3d52f8285c11
Esteban Hernandez <pablo3steban@gmail.com>
2020-07-29 21:06:26 -0600

    fix name db

--- src/api/model/DataConnection.js
+++ src/api/model/DataConnection.js
@@ -4,7 +4,7 @@ const { Client } = require("pg");
 module.exports = class DataConnection {
   constructor() {
     let env = configEnv.db;
-    let database = configEnv.database;
+    let database = env.database;
     let _client = new Client({
       user: env.user,
       host: env.host,
